3eb310601be65a718bfb3dfc732ae10d403e85c5
paluch05 <annapaluch05@gmail.com>
2021-02-13 19:04:18 +0100

    Mail sender task

---
+++ src/main/java/pl/paluchsoft/springmail/AppConfig.java
@@ -0,0 +1,40 @@
+package pl.paluchsoft.springmail;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
+import java.util.Properties;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("wesolakulka1@gmail.com");
+        mailSender.setPassword("hapball1");
+
+//        Properties javaMailProperties = new Properties();
+        Properties javaMailProperties = mailSender.getJavaMailProperties();
+        javaMailProperties.put("mail.smtp.starttls.enable", "true");
+        javaMailProperties.put("mail.smtp.auth", "true");
+        javaMailProperties.put("mail.transport.protocol", "smtp");
+        javaMailProperties.put("mail.debug", "true");
+
+        mailSender.setJavaMailProperties(javaMailProperties);
+        return mailSender;
+    }
+
+//    @Bean
+    public FreeMarkerConfigurationFactoryBean factoryBean() {
+        FreeMarkerConfigurationFactoryBean bean = new FreeMarkerConfigurationFactoryBean();
+        bean.setTemplateLoaderPath("resources/templates/");
+        return bean;
+    }
+}
---
+++ src/main/java/pl/paluchsoft/springmail/Mail.java
@@ -0,0 +1,81 @@
+package pl.paluchsoft.springmail;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class Mail {
+
+    private String mailFrom;
+    private String mailTo;
+    private String mailSubject;
+    private String mailContent;
+    private String contentType;
+    private List<Object> attachments;
+    private Map<String, Object> model;
+    public Mail() {
+        contentType = "text/plain";
+    }
+
+    public String getMailFrom() {
+        return mailFrom;
+    }
+
+    public void setMailFrom(String mailFrom) {
+        this.mailFrom = mailFrom;
+    }
+
+    public String getMailTo() {
+        return mailTo;
+    }
+
+    public void setMailTo(String mailTo) {
+        this.mailTo = mailTo;
+    }
+
+    public String getMailSubject() {
+        return mailSubject;
+    }
+
+    public void setMailSubject(String mailSubject) {
+        this.mailSubject = mailSubject;
+    }
+
+    public String getMailContent() {
+        return mailContent;
+    }
+
+    public void setMailContent(String mailContent) {
+        this.mailContent = mailContent;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public List<Object> getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(List<Object> attachments) {
+        this.attachments = attachments;
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public void setModel(Map<String, Object> model) {
+        this.model = model;
+    }
+
+    public Date getMailSendDate() {
+        return new Date();
+    }
+
+    
+}
