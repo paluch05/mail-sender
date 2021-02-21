c248f64e74dd28ea11d0e251bdfe3b684a9c50d9
paluch05 <annapaluch05@gmail.com>
2021-02-21 21:18:27 +0100

    Task3 templates support

--- src/main/java/pl/paluchsoft/springmailsender/controller/MailController.java
+++ src/main/java/pl/paluchsoft/springmailsender/controller/MailController.java
@@ -1,25 +1,49 @@
 package pl.paluchsoft.springmailsender.controller;
 
+import freemarker.template.TemplateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import pl.paluchsoft.springmailsender.model.Recipient;
 import pl.paluchsoft.springmailsender.service.MailService;
+import pl.paluchsoft.springmailsender.service.RecipientsService;
+import pl.paluchsoft.springmailsender.service.TemplateService;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class MailController {
 
     Logger logger = LoggerFactory.getLogger(MailController.class);
     private final MailService mailService;
+    private final TemplateService templateService;
+    private final RecipientsService recipientsService;
 
-    public MailController(MailService mailService) {
+    public MailController(MailService mailService, TemplateService templateService, RecipientsService recipientsService) {
         this.mailService = mailService;
+        this.templateService = templateService;
+        this.recipientsService = recipientsService;
     }
 
-    @GetMapping("/mail")
-    public void sendMail() {
+    @GetMapping("/mail/{template}/{recipients}")
+    public void sendMail(@PathVariable("template") String template, @PathVariable("recipients") String recipients) throws IOException, TemplateException {
         logger.info("sending email");
-        mailService.sendSimpleMessage("annapaluch05@gmail.com", "rheu", "hfeiuw");
+        List<Recipient> recipientsList;
+        try {
+            recipientsList = recipientsService.getRecipients(recipients);
+        } catch (FileNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
+        for (Recipient recipient : recipientsList) {
+            String renderedTemplate = templateService.getRenderedTemplate(template, recipient.getName());
+            mailService.sendSimpleMessage(recipient.getEmail(), "Mail", renderedTemplate);
+        }
         logger.info("sent email");
     }
 
